a93d1b4c9e18afe7fa54d3e7c91b037c8ebc5f89
Estevao <estevaovasques@hotmail.com>
2021-08-15 12:19:40 -0300

    paginacao

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,8 @@ import Prismic from '@prismicio/client';
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 
+import { useState } from 'react';
+import ApiSearchResponse from '@prismicio/client/types/ApiSearchResponse';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
@@ -29,13 +31,27 @@ interface HomeProps {
 }
 
 export default function Home({ postsPagination }: HomeProps): JSX.Element {
+  const [posts, setPosts] = useState<Post[]>(postsPagination.results);
+  const [nextPage, setNextPage] = useState(postsPagination.next_page);
+
+  function carregarMaisPosts(): void {
+    fetch(
+      `${nextPage}&access_token=${process.env.NEXT_PUBLIC_PRISMIC_ACCESS_TOKEN}`
+    )
+      .then(res => res.json())
+      .then((data: ApiSearchResponse) => {
+        setPosts([...posts, ...data.results]);
+        setNextPage(data.next_page);
+      });
+  }
+
   return (
     <main className={styles.contentContainer}>
       <section className={styles.contentLogo}>
         <img src="/images/logo.svg" alt="Logo da Aplicação" />
       </section>
       <section>
-        {postsPagination.results.map(post => (
+        {posts.map(post => (
           <div className={styles.post} key={post.uid}>
             <h2>{post.data.title}</h2>
             <p>{post.data.subtitle}</p>
@@ -53,9 +69,15 @@ export default function Home({ postsPagination }: HomeProps): JSX.Element {
             </div>
           </div>
         ))}
-        <button className={styles.carregarMaisPosts} type="button">
-          Carregar mais posts
-        </button>
+        {nextPage && (
+          <button
+            onClick={carregarMaisPosts}
+            className={styles.carregarMaisPosts}
+            type="button"
+          >
+            Carregar mais posts
+          </button>
+        )}
       </section>
     </main>
   );
@@ -73,6 +95,7 @@ export const getStaticProps: GetStaticProps = async () => {
         'documento_desafio.author',
       ],
       pageSize: 2,
+      page: 1,
     }
   );
 
